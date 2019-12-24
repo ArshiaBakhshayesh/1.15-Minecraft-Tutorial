7146f195803f1d8c350b43a78dfe717afa3b561c
ChampionAsh5357 <championash5357@gmail.com>
2019-12-24 10:06:15 -0500

    Foods

--- src/main/java/com/championash5357/tutorial/init/TutorialItems.java
+++ src/main/java/com/championash5357/tutorial/init/TutorialItems.java
@@ -6,9 +6,11 @@ import com.championash5357.tutorial.Tutorial;
 import com.championash5357.tutorial.item.ItemRuby;
 import com.championash5357.tutorial.item.ItemTutorialRecord;
 
+import net.minecraft.item.Food;
 import net.minecraft.item.Item;
 import net.minecraft.item.Item.Properties;
 import net.minecraft.item.Rarity;
+import net.minecraft.potion.EffectInstance;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -21,6 +23,11 @@ public class TutorialItems {
 	
 	public static final ItemRuby RUBY = Null();
 	public static final ItemTutorialRecord PERSPECTIVES_RECORD = Null();
+	public static final Item BROCCOLI = Null();
+	
+	public static class Foods {
+		public static final Food BROCCOLI = (new Food.Builder()).hunger(2).saturation(0.2F).fastToEat().effect(new EffectInstance(TutorialEffects.ODD, 10), 0.1F).build();
+	}
 	
 	@Mod.EventBusSubscriber(modid = Tutorial.MOD_ID, bus = Bus.MOD)
 	public static class Register {
@@ -29,7 +36,8 @@ public class TutorialItems {
 		public static void registerItems(final RegistryEvent.Register<Item> event) {
 			final Item[] item = {
 					new ItemRuby(),
-					new ItemTutorialRecord(8, TutorialSounds.Register.getSoundEvent(new ResourceLocation(Tutorial.MOD_ID, "perspectives")), new Properties().maxStackSize(1).group(Tutorial.TUTORIAL_TAB).rarity(Rarity.EPIC), "perspectives_record")
+					new ItemTutorialRecord(8, TutorialSounds.Register.getSoundEvent(new ResourceLocation(Tutorial.MOD_ID, "perspectives")), new Properties().maxStackSize(1).group(Tutorial.TUTORIAL_TAB).rarity(Rarity.EPIC), "perspectives_record"),
+					new Item(new Item.Properties().group(Tutorial.TUTORIAL_TAB).maxStackSize(16).food(Foods.BROCCOLI)).setRegistryName("broccoli")
 			};
 			
 			event.getRegistry().registerAll(item);
